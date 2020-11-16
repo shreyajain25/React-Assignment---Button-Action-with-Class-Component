fbbf8af862291fdd1615c17dc0f42291576c6d08
Shreya <shreyaj2510@gmail.com>
2020-11-17 01:44:25 +0530

    some spelling

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,23 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
-		this.state = {ParaShow: false};
-
-		this.showPara = this.showPara.bind(this);
+		this.state = {ParaShow: false,};
+		
+		this.showPara=this.showPara.bind(this);
 	}
-
+	
 	showPara(){
-    this.setState({ParaShow: true});
+	this.setState({ParaShow: true});
 	}
-
+	
     render() {
     	return(
     		<div id="main">
 				<button id="click" onClick={this.showPara}>Click to see text</button>
 				{this.state.ParaShow?
-				<p id="para">Hello, I've learnt to use the full-stack evaluatsion tool. This makes me so happy</p>
+				<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
 				:null}
-    		</div>
+			</div>
     	);
 	}
 	
