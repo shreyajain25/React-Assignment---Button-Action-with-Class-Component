c4fed970f2b16a340610dae3a4f1e6415094dbaa
Shreya Jain <shreyaj2510@gmail.com>
2020-11-16 17:29:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,20 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
-		this.state = {show: false};
+		this.state = {ParaShow: false};
 
 		this.showPara = this.showPara.bind(this);
 	};
 
 	showPara(){
-    this.setState({show: true});
+    this.setState({ParaShow: true});
 	}
 
     render() {
     	return(
     		<div id="main">
 				<button id="click" onClick={this.showPara}>Click to see text</button>
-				{this.state.show?
+				{this.state.ParaShow?
 				(<p id="para">Hello, I've learnt to use the full-stack evaluatsion tool. This makes me so happy</p>)
 				:null}
     		</div>
